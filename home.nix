dc598936484e4981196ffc8037c8f54f3f272fb7
Ken Klatt <ken@kla.tt>
2022-09-16 22:46:08 -0500

    Bring in the rest of my work git config

--- home.nix
+++ home.nix
@@ -35,21 +35,49 @@
   programs.git = {
     enable = true;
     userName = "Ken Klatt";
-    userEmail = "ken@kla.tt";
+    userEmail = "ken@kla.tt"; # TODO: Make this conditional for work email
     aliases = {
       s = "status";
       sw = "switch";
       d = "diff";
       ds = "diff --staged";
+      # diff with the active branch's upstream
+      du = ''
+        !f() { \
+          git diff `git for-each-ref --format='%(upstream:short)' "$(git symbolic-ref -q HEAD)"`; \
+        }; f
+      '';
       b = "branch";
       ba = "branch -a";
       br = "branch -r";
+      # Shows branches in recently-committed-to order
       brecent = "for-each-ref --sort=committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'";
+      # Used with vim like this: vim $(git conflicts) to open all conflicted files
       conflicts = "diff --name-only --diff-filter=U";
+      # Stage just the whitespace changes
       addwhitespace = "! git add -A && git diff --cached -w | git apply --cached -R";
-      new = "log origin/master@{1}..origin/master@{0}";
+      # Used to 'cd `git root`' quickly
       root = "rev-parse --show-toplevel";
       fix = "commit --amend --no-edit";
+      upstream = ''
+        !f() { \
+          echo `git for-each-ref --format='%(upstream:short)' "$(git symbolic-ref -q HEAD)"`; \
+        }; f
+      '';
+      merge-base = "merge-base"; # This is here for tab completion to find the command. It's part of the plumbing.
+      lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+      lga = "lg --all"; # Include commits from all branches
+      lightweight-tags = ''
+        !f() { \
+          git for-each-ref refs/tags/ --format '%(objecttype) %(refname:short)' | while read ty name; do [ $ty = commit ] && echo $name; done \
+        }; f
+      '';
+      annotated-tags = ''
+        !f() { \
+          git for-each-ref refs/tags/ --format '%(objecttype) %(refname:short)' | while read ty name; do [ $ty = tag ] && echo $name; done \
+        }; f
+      '';
+      remote-tags = "ls-remote --tags origin";
     };
     delta = {
       enable = false;
@@ -66,9 +94,39 @@
     extraConfig = {
       pull = {
         rebase = true;
+        ff = "only";
       };
       merge = {
         ff = false;
+      };
+      core = {
+        # -+F: Configure the pager git uses for diffs to not exit automatically on short
+        # content so that I stop closing my terminal trying to scroll down with ctrl-d.
+        # -j.5: When searching have less put the result in the middle of the screen, not the top.
+        pager = "less -+F -j.5";
+        #pager = vim -R +AnsiEsc -
+        #pager=  vim-pager
+      };
+      clean = {
+        requireForce = false;
+      };
+      fetch = {
+        prune = true;
+      };
+      push = {
+        default = "current";
+        followTags = true;
+        atomic = true;
+      };
+      commit = {
+        # Show the diff when editing the commit message
+        verbose = true;
+      };
+      log = {
+        follow = true;
+      };
+      diff = {
+        noprefix = true;
       };
     };
     ignores = [
