b9b9d58d422bd51121446d58f005a1a176f0e1df
Ken Klatt <ken@kla.tt>
2022-10-09 23:09:12 -0500

    Remove commented configuration and associated explanations
    Do a bit of reordering to hide things that shouldn't be touched often.

--- home.nix
+++ home.nix
@@ -1,20 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
-  home.username = "ken";
-  home.homeDirectory = "/home/ken";
-
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "22.05";
 
   home.packages = with pkgs; [
     ripgrep
@@ -78,10 +64,9 @@
     ];
   };
 
-  programs.lesspipe.enable = true;
 
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
+
+  programs.lesspipe.enable = true;
 
   programs.firefox = {
     enable = true;
@@ -306,6 +291,24 @@
     ];
     extraConfig = builtins.readFile ./dotfiles/vimrc;
   };
+
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "ken";
+  home.homeDirectory = "/home/ken";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.05";
 
 }
 
