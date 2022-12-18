3d45f1b65cb9c9be7e598b1e07160f71865208f3
Ken Klatt <ken@kla.tt>
2022-12-17 21:23:55 -0600

    Add Evil back to Emacs
    It's off by default. Enable with `M-x evil-local-mode`

--- dotfiles/emacs/init.el
+++ dotfiles/emacs/init.el
@@ -1,10 +1,13 @@
-(setq evil-want-C-u-scroll t)
 ;; Set up package.el to work with MELPA
 (require 'package)
 (add-to-list 'package-archives
 	   '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 (package-refresh-contents)
+
+;; Evil
+(require 'evil)
+(setq evil-want-C-u-scroll t)
 
 ;; Enable pdf-tools on-demand (not on startup)
 (pdf-loader-install)
